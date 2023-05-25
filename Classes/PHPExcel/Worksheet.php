8ef34fa2385b2fdc6fb4b8a05746b7c45d941ee3
David Grant <david.grant@theteachercloud.net>
2023-05-25 11:10:24 +0100

    PHP 8 compatibility

--- Classes/PHPExcel/Worksheet.php
+++ Classes/PHPExcel/Worksheet.php
@@ -1474,9 +1474,12 @@ class PHPExcel_Worksheet implements PHPExcel_IComparable
      * @param $pValue PHPExcel_Style_Conditional[]
      * @return PHPExcel_Worksheet
      */
-    public function setConditionalStyles($pCoordinate = 'A1', $pValue)
+    public function setConditionalStyles(string $pCoordinate = 'A1', array $pValue = []): PHPExcel_Worksheet
     {
-        $this->conditionalStylesCollection[strtoupper($pCoordinate)] = $pValue;
+        if(!empty($pValue))
+        {
+            $this->conditionalStylesCollection[strtoupper($pCoordinate)] = $pValue;
+        }
         return $this;
     }
 
